5fef5fab047267f61c8856768bdd9c8e569b7056
Matt2843 <s134532@student.dtu.dk>
2016-01-04 16:27:17 +0100

    Hello

--- 02148_Project/src/engine/fgffcfd.java
+++ 02148_Project/src/engine/fgffcfd.java
@@ -1,5 +1,23 @@
 package engine;
 
-public class fgffcfd {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class fgffcfd {
+	
+	private JFrame frame = new JFrame();
+	
+	public fgffcfd() {
+		
+		frame.setLocationRelativeTo(null);
+		frame.setPreferredSize(new Dimension(400,400));
+		frame.setVisible(true);
+		frame.pack();
+	}
+	
+	public static void main(String[] args) {
+		new fgffcfd();
+	}
+	
 }
