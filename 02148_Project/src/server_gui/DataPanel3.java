f76e09a0ebae5e6c8a3fb76f1429f6e47c4de29d
s134516 <lise_noerby@hotmail.com>
2016-01-05 11:25:23 +0100

    Prøver igen

---
+++ 02148_Project/src/server_gui/DataPanel3.java
@@ -0,0 +1,26 @@
+package server_gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class DataPanel3 extends PanelTemplate{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	void setBackgroundColor() {
+		this.setBackground(Color.white);
+		this.validate();
+	}
+
+	@Override
+	void setPanelSize() {
+		this.setPreferredSize(new Dimension(frameSize.width, (frameSize.height/10)*7));
+		
+	}
+
+	@Override
+	void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+}
