9674afb1ce4618aa6aa79b3423fa2c6fa959a482
Mathias <Mathias@192.168.1.36>
2016-01-06 13:17:11 +0100

    Changes added to ServerSlave

--- 02148_Project/src/server_eng/ServerSlave.java
+++ 02148_Project/src/server_eng/ServerSlave.java
@@ -1,7 +1,10 @@
 package server_eng;
 
+import java.util.HashMap;
+
 public class ServerSlave implements Runnable {
 	private ServerTupleSpace space;
+	private HashMap<String, TaskTemplate> inst_mem = new HashMap<String, TaskTemplate>();
 	
 	public ServerSlave(ServerTupleSpace space) {
 		this.space = space;
