b9e9258f198a02a7f268b63d170d96bd23e5b349
Cap'n Odin <albertjuhl@gmail.com>
2016-01-09 17:00:13 +0100

    Streamlined the output to List<String> and added google status messages to index 0

--- 02148_Project/src/api/GoogleWebApi.java
+++ 02148_Project/src/api/GoogleWebApi.java
@@ -13,69 +13,59 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 public class GoogleWebApi {
 	
-	public static String[] geocoding(String address) throws Exception {
+	static String KEY = "AIzaSyBQGuzq0Ss6n8N1We8zEZ2xy7GwRe0Lpi4";
+	
+	public static List<String> geocoding(String address) throws Exception {
 		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/geocode/xml?address=" + address.replace(" ", "+") + "&key=AIzaSyBQGuzq0Ss6n8N1We8zEZ2xy7GwRe0Lpi4");
+		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/geocode/xml?address=" + address.replace(" ", "+") + "&key=" + KEY);
 		
 		HttpResponse response = httpClient.execute(url);
 		HttpEntity responseEntity = response.getEntity();
 		
 		String xml = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
 		
-		if(!xml.contains("<location>")){
-			return new String[] {"ZERO", "RESULTS"};
-		}
-		String coordxml = xml.split("<location>")[1].split("</location>")[0];
+		List<String> results = new ArrayList<String>();
 		
-		return new String[] {coordxml.split("<lat>")[1].split("</lat>")[0], coordxml.split("<lng>")[1].split("</lng>")[0]};
+		if(xml.contains("<status>")){
+			results.add(xml.split("<status>")[1].split("</status>")[0]);
+		}
+		if(xml.contains("<location>")){
+			String coordxml = xml.split("<location>")[1].split("</location>")[0];
+		
+			results.add(coordxml.split("<lat>")[1].split("</lat>")[0]);
+			results.add(coordxml.split("<lng>")[1].split("</lng>")[0]);
+		}
+		return results;
 	}
 	
 	
-	public static String distMatrix(String dest, String home) throws Exception {
+	public static List<String> distMatrix(String dest, String home) throws Exception {
 		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + home.replace(" ", "+") + "&destinations=" + dest.replace(" ", "+") + "&key=AIzaSyBQGuzq0Ss6n8N1We8zEZ2xy7GwRe0Lpi4");
+		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + home.replace(" ", "+") + "&destinations=" + dest.replace(" ", "+") + "&key=" + KEY);
 		
 		HttpResponse response = httpClient.execute(url);
 		HttpEntity responseEntity = response.getEntity();
 		
 		String xml = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
 		//System.out.println(xml);
-		if(!xml.contains("<distance>")){
-			return "ZERO_RESULTS";
+		
+		List<String> results = new ArrayList<String>();
+		
+		if(xml.contains("<status>")){
+			results.add(xml.split("<status>")[1].split("</status>")[0]);
 		}
-		return xml.split("<distance>")[1].split("</distance>")[0].split("<text>")[1].split("</text>")[0].replace(",", "");
+		
+		if(xml.contains("<distance>")){
+			results.add(xml.split("<distance>")[1].split("</distance>")[0].split("<text>")[1].split("</text>")[0].replace(",", ""));
+		}
+		
+		return results;
 	}
 	
 	
 	public static List<String> seachPlaces(String term) throws Exception {
 		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + term.replace(" ", "+") + ",+Danmark&location=55.676111,12.568333&radius=50000&key=AIzaSyBQGuzq0Ss6n8N1We8zEZ2xy7GwRe0Lpi4");
-		
-		HttpResponse response = httpClient.execute(url);
-		HttpEntity responseEntity = response.getEntity();
-		
-		String xml = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
-		
-		List<String> locals = new ArrayList<String>();
-		
-		if(!xml.contains("<formatted_address>")){
-			locals.add("ZERO_RESULTS");
-			return locals;
-		}
-		
-		for (String res : xml.split("<result>")){
-			if(res.contains("<formatted_address>")){
-				locals.add(res.split("<formatted_address>")[1].split("</formatted_address>")[0]);
-			}
-		}
-		
-		return locals;
-	}
-	
-	
-	public static List<String> seachNearbyPlaces(String term, String[] local) throws Exception {
-		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=" + local[0] + "," + local[1] + "&radius=50000&types=establishment&name=" + term.replace(" ", "+") + "&key=AIzaSyBQGuzq0Ss6n8N1We8zEZ2xy7GwRe0Lpi4");
+		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + term.replace(" ", "+") + ",+Danmark&location=55.676111,12.568333&radius=50000&key=" + KEY);
 		
 		HttpResponse response = httpClient.execute(url);
 		HttpEntity responseEntity = response.getEntity();
@@ -83,20 +73,48 @@ public class GoogleWebApi {
 		String xml = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
 		//System.out.println(xml);
 		
-		List<String> locals = new ArrayList<String>();
+		List<String> results = new ArrayList<String>();
 		
-		if(!xml.contains("<vicinity>")){
-			locals.add("ZERO_RESULTS");
-			return locals;
+		if(xml.contains("<status>")){
+			results.add(xml.split("<status>")[1].split("</status>")[0]);
+		}
+	
+		if(xml.contains("<formatted_address>")){
+			for (String res : xml.split("<result>")){
+				if(res.contains("<formatted_address>")){
+					results.add(res.split("<formatted_address>")[1].split("</formatted_address>")[0]);
+				}
+			}
+		}
+		return results;
+	}
+	
+	
+	public static List<String> seachNearbyPlaces(String term, List<String> local) throws Exception {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpGet url = new HttpGet("https://maps.googleapis.com/maps/api/place/nearbysearch/xml?location=" + local.get(1) + "," + local.get(2) + "&radius=50000&types=establishment&name=" + term.replace(" ", "+") + "&key=" + KEY);
+		
+		HttpResponse response = httpClient.execute(url);
+		HttpEntity responseEntity = response.getEntity();
+		
+		String xml = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
+		//System.out.println(xml);
+		
+		List<String> results = new ArrayList<String>();
+		
+		if(xml.contains("<status>")){
+			results.add(xml.split("<status>")[1].split("</status>")[0]);
 		}
 		
-		for (String res : xml.split("<result>")){
-			if(res.contains("<vicinity>")){
-				locals.add(res.split("<vicinity>")[1].split("</vicinity>")[0]);
+		if(xml.contains("<vicinity>")){
+			for (String res : xml.split("<result>")){
+				if(res.contains("<vicinity>")){
+					results.add(res.split("<vicinity>")[1].split("</vicinity>")[0]);
+				}
 			}
 		}
 		
-		return locals;
+		return results;
 	}
 	
 
