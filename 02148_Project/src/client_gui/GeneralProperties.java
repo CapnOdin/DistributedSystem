573fc9ce5251d71e39c517a9e64d293d6939c33f
s134516 <lise_noerby@hotmail.com>
2016-01-10 22:25:29 +0100

    Ekstra top og bund?

--- 02148_Project/src/client_gui/GeneralProperties.java
+++ 02148_Project/src/client_gui/GeneralProperties.java
@@ -10,5 +10,5 @@ public interface GeneralProperties {
 	public static Dimension frameSize = new Dimension((screenSize.width / 4)*3, (screenSize.height / 4)*3);
 	public static Dimension frameSizePanel1 = new Dimension(frameSize.width, frameSize.height / 20);
 	public static Dimension frameSizePanel2 = new Dimension(frameSize.width, (frameSize.height / 20)*2);
-	public static Dimension frameSizePanel3 = new Dimension(frameSize.width, (frameSize.height / 20)*17);
+	public static Dimension frameSizePanel3 = new Dimension(frameSize.width, (frameSize.height / 20)*16);
 }
--- 02148_Project/src/client_gui/MainFrame.java
+++ 02148_Project/src/client_gui/MainFrame.java
@@ -4,31 +4,32 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.Color;
 
 public class MainFrame extends JFrame implements GeneralProperties{
 	public static Dimension contentPaneSize;
 	public boolean dataPanel = false, connectPanel = false, kalenderPanel = false , oversigtPanel = false;
 	
 	private GridBagConstraints c = new GridBagConstraints();	
-	//private TaskBar tb;
+	private TaskBar tb;
 	private Panel1 p1;
 	private Panel2 p2;
 	private PanelTemplate current_panel;
 	
-	public MainFrame() {		
+	public MainFrame() {	
+		
 		setDefaultProperties();
-		//tb = new TaskBar(this);
+		tb = new TaskBar(this);
 		p1 = new Panel1(this);
 		p2 = new Panel2(this);
 		current_panel = new Panel3Data(this);
-		//addC(tb,0,0,1,1);
+		addC(tb,0,0,1,1);
 		addC(p1,0,1,1,1);
 		addC(p2,0,2,1,1);
 		addC(current_panel,0,3,1,1);
-
 		this.validate();
 	}
 
