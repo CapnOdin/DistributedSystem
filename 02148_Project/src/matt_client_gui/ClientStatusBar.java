1442991f36202ff0fc36b8247ffb55150bafecce
Mathias <Mathias@192.168.1.36>
2016-01-10 20:35:22 +0100

    added fun stuff to status bar of client_gui

--- 02148_Project/src/matt_client_gui/ClientStatusBar.java
+++ 02148_Project/src/matt_client_gui/ClientStatusBar.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -18,7 +19,8 @@ public class ClientStatusBar extends JPanel {
 	private ClientMainFrame parent;
 	private Dimension clientStatusBarSize;
 	
-	private JLabel activePanel = new JLabel("Active Panel: none", JLabel.CENTER);
+	private JLabel activePanel = new JLabel(" Active Panel: none", JLabel.CENTER);
+	private JLabel time = new JLabel("", JLabel.CENTER);
 	
 	public ClientStatusBar(ClientMainFrame parent) {
 		this.parent = parent;
@@ -29,15 +31,17 @@ public class ClientStatusBar extends JPanel {
 	}
 
 	private void addElements() {
-		this.add(activePanel, BorderLayout.CENTER);
+		this.add(activePanel, BorderLayout.WEST);
+		this.add(time, BorderLayout.EAST);
 		this.validate();
 	}
 
 	private void lookForActivePanel() {
-		Timer t = new Timer(2000, new ActionListener() {
+		Timer t = new Timer(1000, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				activePanel.setText("Active Panel: " + parent.getCurrent_panel().getID());
+				activePanel.setText(" Active Panel: " + parent.getCurrent_panel().getID());
+				time.setText(new Date().toString()+ " ");
 			}
 		});
 		t.start();	
