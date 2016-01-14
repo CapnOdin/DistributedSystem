0df57b47210cd53451ce38aac6ee8fbe3dda1d5c
Mathias <Mathias@192.168.1.36>
2016-01-14 20:15:44 +0100

    123

--- 02148_Project/src/server_gui/ServerMainFrame.java
+++ 02148_Project/src/server_gui/ServerMainFrame.java
@@ -17,7 +17,7 @@ public class ServerMainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	private Dimension frameSize = new Dimension((int)screenSize.getWidth()/2,(int)screenSize.getHeight()/2);
+	private Dimension frameSize = new Dimension(1024,600);
 	
 	private GridBagConstraints c = new GridBagConstraints();
 	
