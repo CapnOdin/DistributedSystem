bec9e1aafc13a73bfa2e9db67005f33e4c74b669
CapnOdin <chip-10@hotmail.com>
2016-01-15 13:06:05 +0100

    removed error messages

--- 02148_Project/src/api/MomentApi.java
+++ 02148_Project/src/api/MomentApi.java
@@ -24,67 +24,82 @@ public class MomentApi {
 		HttpResponse response;
 		HttpEntity responseEntity;
 		Map<String, String> addressDict;
-		String address = "";
+		String address = "", vikarGUID = "", str = "";
 		
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		
-		// logging in via Post
-		uploadFile = new HttpPost("https://www.moment.dk/staff/login");
-		builder = MultipartEntityBuilder.create();
-		builder.addTextBody("UserName", email, ContentType.TEXT_PLAIN);
-		builder.addTextBody("Password", pass, ContentType.TEXT_PLAIN);
-		builder.addTextBody("action:Login", "", ContentType.TEXT_PLAIN);
-		builder.addTextBody("ufprt", "E72A01AEA2F5FCC58E32479C3522A99D1D3C973E5B6D6C7787CCBA10E374FCF67555CA701AAF324756BB11DBBB9C43401EB3B9D9446CC1A3D725DAC6052CA8A5D046E4EE11C3A9EA247CC5B9954FCF16AFDDDA49517A55728EEE04719F719CB1F2B3CCE43B7027F5AFF22B510A9B2E2CC803922180E74C65D7067CC31F58124232E46316B73A2FE6C2B5151964AF6573C1BFEDD0194474FEF69B026504F4E1A3", ContentType.TEXT_PLAIN);
-		HttpEntity multipart = builder.build();
-		uploadFile.setEntity(multipart);
-		httpClient.execute(uploadFile);
+		try{
+			// logging in via Post
+			uploadFile = new HttpPost("https://www.moment.dk/staff/login");
+			builder = MultipartEntityBuilder.create();
+			builder.addTextBody("UserName", email, ContentType.TEXT_PLAIN);
+			builder.addTextBody("Password", pass, ContentType.TEXT_PLAIN);
+			builder.addTextBody("action:Login", "", ContentType.TEXT_PLAIN);
+			builder.addTextBody("ufprt", "E72A01AEA2F5FCC58E32479C3522A99D1D3C973E5B6D6C7787CCBA10E374FCF67555CA701AAF324756BB11DBBB9C43401EB3B9D9446CC1A3D725DAC6052CA8A5D046E4EE11C3A9EA247CC5B9954FCF16AFDDDA49517A55728EEE04719F719CB1F2B3CCE43B7027F5AFF22B510A9B2E2CC803922180E74C65D7067CC31F58124232E46316B73A2FE6C2B5151964AF6573C1BFEDD0194474FEF69B026504F4E1A3", ContentType.TEXT_PLAIN);
+			HttpEntity multipart = builder.build();
+			uploadFile.setEntity(multipart);
+			httpClient.execute(uploadFile);
+		} catch (Exception e) {
+			
+		}
 		
+		try{
+			// Geting vikarGUID
+			HttpGet getvikarGUID = new HttpGet("https://www.moment.dk/vikar/profil/stamdata/personlige-data/");
+			response = httpClient.execute(getvikarGUID);
+			responseEntity = response.getEntity();
+			str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
+			int index = str.indexOf("var studGuid = (\"");
+			vikarGUID = str.substring(index, index + 80);
+			vikarGUID = vikarGUID.substring(vikarGUID.indexOf("(\"") + 2, vikarGUID.indexOf("\")"));
+			System.out.println(vikarGUID);
+		} catch (Exception e) {
+			
+		}
 		
-		// Geting vikarGUID
-		HttpGet getvikarGUID = new HttpGet("https://www.moment.dk/vikar/profil/stamdata/personlige-data/");
-		response = httpClient.execute(getvikarGUID);
-		responseEntity = response.getEntity();
-		String str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
-		int index = str.indexOf("var studGuid = (\"");
-		String vikarGUID = str.substring(index, index + 80);
-		vikarGUID = vikarGUID.substring(vikarGUID.indexOf("(\"") + 2, vikarGUID.indexOf("\")"));
-		System.out.println(vikarGUID);
-		
-		// Geting user data
-		HttpGet getuserData = new HttpGet("https://www.moment.dk/api/Validated/GetStudentBaseProfile/"+ vikarGUID +"?_=1452272073212");
-		response = httpClient.execute(getuserData);
-		responseEntity = response.getEntity();
-		str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
-		
-		String[] dics = str.replaceAll("\"", "").replace("{", "").split("},");
-		
-		for (String dic : dics) {
-			if(dic.startsWith("MAddress")){
-				addressDict = generateDictionarie(dic.substring(9));
-				address = addressDict.get("Street") + " " + addressDict.get("StreetNr") + ", " + addressDict.get("ZipCode") + " " + addressDict.get("City");
-				break;
+		try{
+			// Geting user data
+			HttpGet getuserData = new HttpGet("https://www.moment.dk/api/Validated/GetStudentBaseProfile/"+ vikarGUID +"?_=1452272073212");
+			response = httpClient.execute(getuserData);
+			responseEntity = response.getEntity();
+			str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
+			
+			String[] dics = str.replaceAll("\"", "").replace("{", "").split("},");
+			
+			for (String dic : dics) {
+				if(dic.startsWith("MAddress")){
+					addressDict = generateDictionarie(dic.substring(9));
+					address = addressDict.get("Street") + " " + addressDict.get("StreetNr") + ", " + addressDict.get("ZipCode") + " " + addressDict.get("City");
+					break;
+				}
 			}
+		} catch (Exception e) {
+			
 		}
 		return new String[] {vikarGUID, address};
 	}
 	
 	
 	public static List<Map<String, String>> getVagter(String date, String vikarGUID) throws Exception {
+		String vagter = "{\"state\":\"Error\"}";
+		
 		HttpClient httpClient = HttpClientBuilder.create().build();
-		
-		HttpPost poster = new HttpPost("https://www.moment.dk/api/VikarCalendar/GetConsolidatedListOfVagtTasks/1");
-		
-		StringEntity params = new StringEntity("{\"vikarGUID\":\"" + vikarGUID + "\",\"start\":\"" + date + "T00:00:00.000Z\",\"viewSideBySideBPData\":\"false\"}");
-		
-		poster.addHeader("Content-type","application/json");
-		poster.setEntity(params);
-		HttpResponse response = httpClient.execute(poster);
-		HttpEntity responseEntity = response.getEntity();
-		
-		String str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
-		
-		String vagter = str.substring(1, str.length() - 2);
-		
+		try{
+			HttpPost poster = new HttpPost("https://www.moment.dk/api/VikarCalendar/GetConsolidatedListOfVagtTasks/1");
+			
+			StringEntity params = new StringEntity("{\"vikarGUID\":\"" + vikarGUID + "\",\"start\":\"" + date + "T00:00:00.000Z\",\"viewSideBySideBPData\":\"false\"}");
+			
+			poster.addHeader("Content-type","application/json");
+			poster.setEntity(params);
+			HttpResponse response = httpClient.execute(poster);
+			HttpEntity responseEntity = response.getEntity();
+			
+			String str = IOUtils.toString(responseEntity.getContent(), StandardCharsets.UTF_8);
+			
+			vagter = str.substring(1, str.length() - 2);
+		} catch (Exception e) {
+			
+		}
 		return generateListOfDictionaries(vagter);
 	}
 	
